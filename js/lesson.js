5ad68c7cc8a49ccd6c7e33dd8a60b5e6697557e9
Sinkvane <69595744+sinkvane@users.noreply.github.com>
2022-11-21 23:42:31 +0700

    practice 3

--- js/lesson.js
+++ js/lesson.js
@@ -1,16 +1,1 @@
 "use strict";
-
-function fifthTask() {
-  const arrayOfNumbers = [];
-
-  for (let i = 5; i <= 10; i++) {
-    arrayOfNumbers[i - 5] = i;
-  }
-  console.log(arrayOfNumbers);
-
-
-  // Не трогаем
-  return arrayOfNumbers;
-}
-
-fifthTask();
